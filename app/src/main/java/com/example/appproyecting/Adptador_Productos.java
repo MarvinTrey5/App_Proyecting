84ea4f82e222ec89b1476189506a0da818348d37
MarvinTrey5 <santosmarvin201@gmail.com>
2023-05-31 09:46:49 -0600

    Se agregó la parte del buscador.

---
+++ app/src/main/java/com/example/appproyecting/Adptador_Productos.java
@@ -0,0 +1,64 @@
+package com.example.appproyecting;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adptador_Productos extends RecyclerView.Adapter<Adptador_Productos.ViewHolder> {
+    List<ProductosPlanilla> Product;
+    LayoutInflater MiInflate;
+    Context Datos;
+
+    public Adptador_Productos(List<ProductosPlanilla> product, Context datos) {
+        Product = product;
+        MiInflate = LayoutInflater.from(datos);
+        Datos = datos;
+    }
+
+    @NonNull
+    @Override
+    public Adptador_Productos.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View Observar = MiInflate.inflate(R.layout.planilla_productos,null);
+        return new Adptador_Productos.ViewHolder(Observar);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adptador_Productos.ViewHolder holder, int position) {
+        holder.binData(Product.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return Product.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView FotoP;
+        TextView NombreP, Pre_Pro, Canti_Pr, Valo_Prd, Desc_Prd;
+        ViewHolder(View Info ){
+            super(Info);
+            FotoP = Info.findViewById(R.id.Imag_P);
+            NombreP = Info.findViewById(R.id.Nom_P);
+            Pre_Pro = Info.findViewById(R.id.Precio_P);
+            Canti_Pr = Info.findViewById(R.id.Cant_Pro);
+            Valo_Prd = Info.findViewById(R.id.Valorac_P);
+            Desc_Prd = Info.findViewById(R.id.Descrip_P);
+        }
+        public void binData(final ProductosPlanilla Obtener){
+            FotoP.setImageResource(Obtener.getVerFoto());
+            NombreP.setText(Obtener.getNom());
+            Pre_Pro.setText(Obtener.getPrec());
+            Canti_Pr.setText(Obtener.getCant());
+            Valo_Prd.setText(Obtener.getVal());
+            Desc_Prd.setText(Obtener.getDes());
+        }
+    }
+}
--- app/src/main/java/com/example/appproyecting/MainActivity.java
+++ app/src/main/java/com/example/appproyecting/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.appproyecting;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,12 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.appproyecting.ui.main.SectionsPagerAdapter;
 import com.example.appproyecting.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Go;
     private ActivityMainBinding binding;
 
     @Override
@@ -31,8 +33,17 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+
+        Go = findViewById(R.id.Buscar);
         tabs.getTabAt(0).setIcon(R.drawable.masv_1);
         tabs.getTabAt(1).setIcon(R.drawable.califica);
         tabs.getTabAt(2).setIcon(R.drawable.prom);
+        Go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Venta_Go = new Intent(MainActivity.this, Buscar.class);
+                startActivity(Venta_Go);
+            }
+        });
     }
 }
