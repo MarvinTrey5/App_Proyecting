f021daf8afdf7b27a372fa2bf26281647a3e94da
MarvinTrey5 <santosmarvin201@gmail.com>
2023-05-31 23:19:18 -0600

    Se agregó un listview que nos mostrará el usuario y el comentario del cliente. By Miguel Elias.

--- app/src/main/java/com/example/appproyecting/Enviar_Comentario.java
+++ app/src/main/java/com/example/appproyecting/Enviar_Comentario.java
@@ -6,61 +6,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Enviar_Comentario extends AppCompatActivity {
 
-    EditText commentEditText;
+    EditText Nom_Usuario, ComentUser;
     Button sendButton;
-    TextView commentsTextView;
+    ListView Ver_lis;
     ImageView Ir_Base;
+    List<String> User_Visual;
+    ArrayAdapter<String> adapter;
+    String ID_user, Com_User, Ver_Nom_coment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_enviar_comentario);
-        commentEditText = findViewById(R.id.commentEditText);
+        Nom_Usuario = findViewById(R.id.User_EditText);
+        ComentUser = findViewById(R.id.Comment_EditText);
         sendButton = findViewById(R.id.sendButton);
-        commentsTextView = findViewById(R.id.commentsTextView);
+        Ver_lis = findViewById(R.id.Ver_Lista);
+        User_Visual = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, User_Visual);
+        Ver_lis.setAdapter(adapter);
+
         Ir_Base = findViewById(R.id.Volver_Visualizar);
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String comment = commentEditText.getText().toString();
-
-                // Aquí puedes agregar código para subir el comentario a la base de datos
-                // Ejemplo usando Firebase Realtime Database:
-                DatabaseReference commentsRef = FirebaseDatabase.getInstance().getReference("comments");
-                String commentId = commentsRef.push().getKey();
-                commentsRef.child(commentId).setValue(comment);
-
-                // Actualizar el TextView para mostrar los comentarios existentes
-                commentsRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        StringBuilder commentsBuilder = new StringBuilder();
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            String comment = snapshot.getValue(String.class);
-                            commentsBuilder.append(comment).append("\n");
-                        }
-                        commentsTextView.setText(commentsBuilder.toString());
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        // Manejar el error si es necesario
-                    }
-                });
-            }
-        });
         Ir_Base.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,5 +53,20 @@ public class Enviar_Comentario extends AppCompatActivity {
                 startActivity(Ir_Inicio);
             }
         });
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ID_user = Nom_Usuario.getText().toString();
+                Com_User = ComentUser.getText().toString();
+                Ver_Nom_coment = ID_user + " : \n " + Com_User;
+                agregar(Ver_Nom_coment);
+            }
+        });
+    }
+    public void agregar(String id_usuario){
+        User_Visual.add(id_usuario);
+        adapter.notifyDataSetChanged();
+        Nom_Usuario.getText().clear();
+        ComentUser.getText().clear();
     }
 }
