d3fe3c708fb0746007d1a745d105794c65913576
MarvinTrey5 <santosmarvin201@gmail.com>
2023-06-07 22:12:22 -0600

    Se agregaron modificaron ciertos datos al enviar la valoración.By Brandon

--- app/src/main/java/com/example/appproyecting/Recibir_Coments.java
+++ app/src/main/java/com/example/appproyecting/Recibir_Coments.java
@@ -15,31 +15,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Recibir_Coments extends AppCompatActivity {
-    ListView Ver_DatosComents, Ver_Valoracion;
+    ListView Ver_DatosComents;
     Button Volver;
-    ArrayList<String> Obtner_Datos, ratingList;
-    ArrayAdapter<String> Adap_tador1, Adap_tador2;
+    ArrayList<String> Obtner_Datos;
+    ArrayAdapter<String> Adap_tador1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recibir_coments);
         Ver_DatosComents = findViewById(R.id.Observar_Coments);
-        Ver_Valoracion = findViewById(R.id.Observar_Validacion);
+        ListView Ver_Valoracion = findViewById(R.id.Observar_Validacion);
         Volver =findViewById(R.id.Vol_ver);
         Obtner_Datos = new ArrayList<>();
-        ratingList = new ArrayList<>();
         Adap_tador1 = new ArrayAdapter<String>(Recibir_Coments.this, android.R.layout.simple_list_item_1,Obtner_Datos);
         Ver_DatosComents.setAdapter(Adap_tador1);
-        Adap_tador2 = new ArrayAdapter<>(Recibir_Coments.this, android.R.layout.simple_list_item_1, ratingList);
-        Ver_Valoracion.setAdapter(Adap_tador2);
-        Intent Ver_Datos = getIntent();
-        String Ver_Coments_US = Ver_Datos.getStringExtra("Ver_Coment");
-        Add(Ver_Coments_US);
-        String ratingText = getIntent().getStringExtra("ratingText");
-        if (ratingText != null) {
-            ratingList.add(ratingText);
-           Adap_tador2.notifyDataSetChanged();
-        }
+        float rating = getIntent().getFloatExtra("rating", 0.0f);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(Recibir_Coments.this, android.R.layout.simple_list_item_1, new String[]{String.valueOf(rating)});
+        Ver_Valoracion.setAdapter(adapter);
         Volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
